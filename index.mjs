28f9eb8b9ea9765bf169ef1ecb02f79a2642bfcc
Peter Droogmans (attiks) <peter@attiks.com>
2024-10-04 14:11:39 +0200

    feat: Add flags

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import { Parser } from '@json2csv/plainjs';
 import * as fs from 'fs';
 import { XMLParser } from "fast-xml-parser";
+import * as iso3_2 from "country-iso-3-to-2";
 
 const startYear = 2008;
 const endYear = new Date().getFullYear() + 1;
@@ -74,8 +75,16 @@ async function writeCsv(results, year, needsHeader) {
                 value: 'DonorName'
             },
             {
-                label: 'CountryCode',
-                value: 'CountryCode'
+                label: 'DonorNameWithFlag',
+                value: (row) => (iso3_2.default(row.CountryCode) ? ':' + iso3_2.default(row.CountryCode).toLowerCase() + ': ' : '') + row.DonorName
+            },
+            {
+                label: 'Iso3',
+                value: (row) => (row.CountryCode ? row.CountryCode.toLowerCase() : '')
+            },
+            {
+                label: 'Iso2',
+                value: (row) => (iso3_2.default(row.CountryCode) ? iso3_2.default(row.CountryCode).toLowerCase() : '')
             },
             {
                 label: 'Earmarked',
@@ -112,12 +121,13 @@ async function writeCsv(results, year, needsHeader) {
 
     for (const year of years) {
         console.info('Processing ' + year);
+
         let yearStream = fs.createWriteStream('./data_' + year + '.csv');    
         let XMLdata = await getData(year);
         
         let jObj = parser.parse(XMLdata);
         let data = jObj['soap:Envelope']['soap:Body'].GetDonorRankingForOCHAOnlineV2Response.GetDonorRankingForOCHAOnlineV2Result.Donors.DonorRankV2 || [];
-    
+
         let csv = await writeCsv(data, year, needsHeader);
         combinedStream.write(csv);
         combinedStream.write("\n");
